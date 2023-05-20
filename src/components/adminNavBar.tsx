dc0f1a8ebd3daa2162571a8689f180b801394b39
Abhishek <abhishekroxtomar@gmail.com>
2023-05-21 00:14:59 +0530

    ui fix

--- src/components/adminNavBar.tsx
+++ src/components/adminNavBar.tsx
@@ -16,29 +16,46 @@ const AdminNavBar = () => {
         </a>
         {/* nav links */}
         <div className="flex mt-5 flex-col space-y-1 justify-center items-center">
-            <div onClick={(e) => handleClick(e)} id={'Dashboard'} className={active=='Dashboard'?"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-200 bg-blue-100 rounded-xl transition-all duration-300 ease-linear ":"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-100 rounded-xl transition-all duration-300 ease-linear "}>
-                <FontAwesomeIcon icon={faChartBar} className="mr-2" />
-                <p className="text-sm font-semibold pointer-events-none">Dashboard</p>
+            <div onClick={(e) => handleClick(e)} id={'Dashboard'} className={active=='Dashboard'?"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-200 text-blue-400 bg-blue-100 rounded-xl transition-all duration-300 ease-linear ":"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-100 rounded-xl transition-all duration-300 ease-linear "} >
+                <FontAwesomeIcon icon={faChartBar} className={active=='Dashboard'?"mr-2 text-blue-400" :"mr-2 text-gray-500"} />
+                <p
+                
+                className={active=='Dashboard'?"text-sm font-semibold pointer-events-none":"text-sm font-semibold pointer-events-none text-gray-500 " }
+                 >Dashboard</p>
             </div>
-            <div onClick={(e) => handleClick(e)} id={'Statistics'} className={active=='Statistics'?"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-200 bg-blue-100 rounded-xl transition-all duration-300 ease-linear ":"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-100 rounded-xl transition-all duration-300 ease-linear "}>
-                <FontAwesomeIcon icon={faClipboardList} className="mr-2" />
-                <p className="text-sm font-semibold pointer-events-none">Statistics</p>
+            <div onClick={(e) => handleClick(e)} id={'Statistics'} className={active=='Statistics'?"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-200 text-blue-400 bg-blue-100 rounded-xl transition-all duration-300 ease-linear ":"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-100 rounded-xl transition-all duration-300 ease-linear "} >
+                <FontAwesomeIcon icon={faClipboardList} className={active=='Statistics'?"mr-2 text-blue-400" :"mr-2 text-gray-500"} />
+                <p
+                
+                className={active=='Statistics'?"text-sm font-semibold pointer-events-none":"text-sm font-semibold pointer-events-none text-gray-500 " }
+                 >Statistics</p>
             </div>
-            <div onClick={(e) => handleClick(e)} id={'Bookings'} className={active=='Bookings'?"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-200 bg-blue-100 rounded-xl transition-all duration-300 ease-linear ":"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-100 rounded-xl transition-all duration-300 ease-linear "}>
-                <FontAwesomeIcon icon={faPlane} className="mr-2" />
-                <p className="text-sm font-semibold pointer-events-none">Bookings</p>
+            <div onClick={(e) => handleClick(e)} id={'Bookings'} className={active=='Bookings'?"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-200 text-blue-400 bg-blue-100 rounded-xl transition-all duration-300 ease-linear ":"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-100 rounded-xl transition-all duration-300 ease-linear "} >
+                <FontAwesomeIcon icon={faPlane} className={active=='Bookings'?"mr-2 text-blue-400" :"mr-2 text-gray-500"} />
+                <p
+                
+                className={active=='Bookings'?"text-sm font-semibold pointer-events-none":"text-sm font-semibold pointer-events-none text-gray-500 " }
+                 >Bookings</p>
             </div>
-            <div onClick={(e) => handleClick(e)} id={'Aircrafts'} className={active=='Aircrafts'?"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-200 bg-blue-100 rounded-xl transition-all duration-300 ease-linear ":"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-100 rounded-xl transition-all duration-300 ease-linear "}>
-                <FontAwesomeIcon icon={faPlane} className="mr-2" />
-                <p className="text-sm font-semibold pointer-events-none">Aircrafts</p>
+            <div onClick={(e) => handleClick(e)} id={'Aircrafts'} className={active=='Aircrafts'?"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-200 text-blue-400 bg-blue-100 rounded-xl transition-all duration-300 ease-linear ":"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-100 rounded-xl transition-all duration-300 ease-linear "} >
+                <FontAwesomeIcon icon={faPlane} className={active=='Aircrafts'?"mr-2 text-blue-400" :"mr-2 text-gray-500"} />
+                <p
+                
+                className={active=='Aircrafts'?"text-sm font-semibold pointer-events-none":"text-sm font-semibold pointer-events-none text-gray-500 " }
+                 >Aircrafts</p>
             </div>
-            <div onClick={(e) => handleClick(e)} id={'Users'} className={active=='Users'?"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-200 bg-blue-100 rounded-xl transition-all duration-300 ease-linear ":"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-100 rounded-xl transition-all duration-300 ease-linear "}>
-                <FontAwesomeIcon icon={faUser} className="mr-2" />
-                <p className="text-sm font-semibold pointer-events-none">Users</p>
+            <div onClick={(e) => handleClick(e)} id={'Users'} className={active=='Users'?"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-200 bg-blue-100 text-blue-400 rounded-xl transition-all duration-300 ease-linear ":"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-100 rounded-xl transition-all duration-300 ease-linear "} >
+                <FontAwesomeIcon icon={faUser} className={active=='Users'?"mr-2 text-blue-400" :"mr-2 text-gray-500"} />
+                <p
+                className={active=='Users'?"text-sm font-semibold pointer-events-none":"text-sm font-semibold pointer-events-none text-gray-500 " }
+                 >Users</p>
             </div>
-            <div onClick={(e) => handleClick(e)}id={'Settings'}  className={active=='Settings'?"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-200 bg-blue-100 rounded-xl transition-all duration-300 ease-linear ":"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-100 rounded-xl transition-all duration-300 ease-linear "}>
-                <FontAwesomeIcon icon={faCogs} className="mr-2" />
-                <p className="text-sm font-semibold pointer-events-none">Settings</p>
+            <div onClick={(e) => handleClick(e)}id={'Settings'}  className={active=='Settings'?"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-200 text-blue-400 bg-blue-100 rounded-xl transition-all duration-300 ease-linear ":"flex justify-center items-center w-[150px] h-[50px] hover:bg-blue-100 rounded-xl transition-all duration-300 ease-linear "} >
+                <FontAwesomeIcon icon={faCogs} className={active=='Settings'?"mr-2 text-blue-400" :"mr-2 text-gray-500"} />
+                <p
+                
+                className={active=='Settings'?"text-sm font-semibold pointer-events-none":"text-sm font-semibold pointer-events-none text-gray-500 " }
+                 >Settings</p>
             </div>
             </div>
     </div>
--- src/components/dashboardOverview.tsx
+++ src/components/dashboardOverview.tsx
@@ -4,26 +4,26 @@ const DashboardOverview = ({bookings, emptySeats,cancelledSeats,totalRevenue}) =
   return (
     <div className="flex justify-around mt-8 mx-4 h-[10vh]">
         <div className="flex flex-col items-center bg-white p-4 rounded-xl shadow hover:scale-105 transition-all duration-300 ease-linear cursor-default">
-            <div className="text-lg font-bold text-blue-700">{bookings}</div>
-            <div className="text-sm">Number of Bookings Today </div>
+            <div className="text-lg font-bold text-blue-400">{bookings}</div>
+            <div className="text-sm text-gray-500">Number of Bookings Today </div>
         </div>
         <div className="flex flex-col items-center bg-white p-4 rounded-xl shadow hover:scale-105 transition-all duration-300 ease-linear cursor-default">
-            <div className="text-lg font-bold text-blue-700">
+            <div className="text-lg font-bold text-blue-400">
             {emptySeats}
             </div>
-            <div className="text-sm">Number of Empty Seats Today </div>
+            <div className="text-sm text-gray-500">Number of Empty Seats Today </div>
         </div>
         <div className="flex flex-col items-center bg-white p-4 rounded-xl shadow hover:scale-105 transition-all duration-300 ease-linear cursor-default">
-            <div className="text-lg font-bold text-blue-700">
+            <div className="text-lg font-bold text-blue-400">
             {cancelledSeats}
             </div>
-            <div className="text-sm">Bookings cancelled Today </div>
+            <div className="text-sm text-gray-500">Bookings cancelled Today </div>
         </div>
         <div className="flex flex-col items-center bg-white p-4 rounded-xl shadow hover:scale-105 transition-all duration-300 ease-linear cursor-default">
-            <div className="text-lg font-bold text-blue-700">
+            <div className="text-lg font-bold text-blue-400">
             {totalRevenue}
             </div>
-            <div className="text-sm">Total Revenue Today </div>
+            <div className="text-sm text-gray-500">Total Revenue Today </div>
         </div>
     </div>
   )
