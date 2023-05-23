d53cc16a0d63c8af0a234a69f6dcc04ff0695fd6
Abhishek <abhishekroxtomar@gmail.com>
2023-05-23 15:41:32 +0530

    userAvatar setup

--- src/components/navBar.tsx
+++ src/components/navBar.tsx
@@ -9,13 +9,16 @@ const NavBar = () => {
   const userInfo = useSelector((state:any) => state.auth.userInfo) 
   const userToken = useSelector((state:any) => state.auth.token)  
 
+  const logOutUser = () => {
+    dispatch(deleteData())
+  }
+
   useEffect(() => { 
     console.log('useEffect')
     if(userInfo === '' && userToken === '' ){
-      console.log("empty : ",userInfo,userToken)
+      console.log("empty")
     }else{
-      console.log("not empty : ",userInfo)
-      console.log("not empty : ",userToken)
+      console.log("not empty")
     }
   }, [])
 
@@ -28,16 +31,19 @@ const NavBar = () => {
       {/* T0D0 LINK to registration page -> pass specific data eg reg/log to open register/login card */}
       {userInfo !== '' ? 
       (
-        <>
-        <div className={`rounded-full h-10 w-10 bg-slate-100 hover:scale-110 transition-all ease-in-out duration-400`}></div>
-        <div className=' cursor-default' >{userInfo?.displayName}</div>
-        </>
+        <div className='flex space-x-2 ml-10 justify-center items-center'>
+          <img className="inline-block h-10 w-10 rounded-full hover:ring-2 hover:ring-blue-400 transition-all ease-linear duration-300 " src={`${userInfo?.photoURL}`} alt={`${userInfo?.displayName}`}/>
+          <div>
+            <div className='cursor-default' >{userInfo?.displayName}</div>
+            <div onClick={() => logOutUser()} className='text-xs hover:animate-pulse cursor-pointer' >Logout</div>
+          </div>
+        </div>
       )  
       : (
-        <>
+        <div className='flex space-x-5 ml-10 justify-center items-center'>
           <Link className='hover:scale-110 transition-all ease-in-out duration-300 underline' to={{ pathname: '/signUpLogin' }} state={{ login: true }}>Sign up</Link>
           <Link className='hover:scale-110 transition-all ease-in-out duration-300' to={{ pathname: '/signUpLogin' }} state={{ login: false }}>Login</Link>
-        </>
+        </div>
       )}
     </div>
   </div>
