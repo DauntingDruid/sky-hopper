63586dec69ef1a3574703bcc8b21daf324254941
Abhishek <abhishekroxtomar@gmail.com>
2023-05-30 13:03:39 +0530

    airplane loader booking page, bkin cnf page

--- src/components/flightDetailsCard.tsx
+++ src/components/flightDetailsCard.tsx
@@ -1,8 +1,16 @@
 import { faPlaneDeparture } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
+import airplaneLoading from '../assets/lottie/airplane-loading-purple.json'
+import { useLottie } from 'lottie-react'
 
-const FlightDetailsCard = ({flightInfo}) => {
+
+const FlightDetailsCard = ({flightInfo}) => { 
+  const loading = {
+    animationData: airplaneLoading,
+    loop: true
+  };
+  const { View :airplaneLoad} = useLottie(loading);
   return (
     <div className='h-[25vh] mt-10 container z-10 rounded-lg bg-white bg-opacity-70 py-5 shadow-md'>
             <div className='flex h-[10%] items-center px-8 mb-3'>
@@ -25,12 +33,13 @@ const FlightDetailsCard = ({flightInfo}) => {
                   </div>  
                   <div className='flex flex-row justify-center items-center'>
                     {/* flight duration */}
-                    <hr className='w-10 h-1 bg-gray-500' />
                     <div className='flex flex-col justify-center items-center'>
-                      <p className='text-2xl font-medium'>{flightInfo.duration}</p>
-                      <p className='text-sm font-medium text-gray-500'>Non-stop</p>
+                      <p className='text-2xl font-medium mb-5'>{flightInfo.duration}</p>
+                      <p className='text-sm font-medium text-gray-500 mt-2'>Non-stop</p>
+                      <div className='absolute z-50'>
+                      {airplaneLoad}
+                      </div>
                     </div>
-                      <hr className='w-10 h-1 bg-gray-500' />
                   </div>
                   <div className='w-1/3 flex flex-row justify-center items-center'>
                     {/* flight arrival timing */}
--- src/pages/bookingConfirmationPage.tsx
+++ src/pages/bookingConfirmationPage.tsx
@@ -1,8 +1,34 @@
+import { useLottie } from 'lottie-react';
+import passportLottie from '../assets/lottie/passport.json';
 import React from 'react'
+import NavBar from '../components/navBar';
 
 const BookingConfirmationPage = () => {
+
+  const passportAnimation = {
+    animationData: passportLottie,
+    loop: false
+  };
+  const { View :passport} = useLottie(passportAnimation);
+
   return (
-    <div>BookingConfirmationPage</div>
+    <>
+      <NavBar />
+      <div className='flex flex-col justify-between items-center h-screen w-screen'>
+        <div className='absolute z-0'>
+          {passport}
+        </div>
+        <div className='h-full w-full flex flex-col justify-between items-center z-10'>
+          <div className='h-1/3 flex flex-col justify-center items-center xs:px-4 text-4xl text-gray-600 '>
+            <p>Your flight is booked and confirmed! </p> 
+            <p>We can't wait to serve you on board. Bon voyage!</p>
+          </div>
+          <div className='h-1/3 flex flex-col justify-center items-center xs:px-4 text-4xl text-gray-600 '>
+            <button className='w-fit h-fit p-3 rounded-3xl bg-blue-300 hover:bg-blue-400 active:bg-blue-500 transition-all duration-200 ease-in-out hover:scale-105 active:scale-95 '>Continue exploring</button> 
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
